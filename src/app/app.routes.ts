4798ab8f19a5c1946f6ed15a189668744fefc0ca
Walterr4 <45957500+Walterr4@users.noreply.github.com>
2019-01-29 17:15:46 -0500

    SONQO2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Route} from '@angular/router';
 import { PageNotFoundComponent } from './component/shared/page-not-found/page-not-found.component';
 import { PageComponent } from './component/pages/page/page.component';
+import { SomosComponent } from './component/pages/somos/somos.component';
+import { HomeComponent } from './component/pages/home/home.component';
 
 const routerPaths:Route[] = [
-    { path  : '', component: PageComponent },
+    { path  : '', component: HomeComponent },
     { path  : '**', component: PageNotFoundComponent }
     
 ];
